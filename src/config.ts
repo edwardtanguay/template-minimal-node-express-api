85bd861247208ecc40aab699b6cc36c89e493fb5
Edward Tanguay (windows) <edward@tanguay.info>
2024-02-20 21:17:33 +0100

    minimized

--- src/config.ts
+++ src/config.ts
@@ -1,12 +1,3 @@
-import { IApiStatus } from './interfaces.js';
-
 export const getPort = () => {
 	return 4206;
-}
-
-export const apiStatus = (): IApiStatus => {
-	return {
-		status: "live",
-		whenLiveAgainDateTime: "2024-01-21 14:00:00"
-	}
 }
